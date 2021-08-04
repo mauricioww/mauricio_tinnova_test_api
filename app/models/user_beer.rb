753676e9e8bf0d3c7092142aaf6efb70f06045a9
Mauricio <marcos_c79@hotmail.com>
2021-08-04 16:00:03 -0500

    migrations and models

---
+++ app/models/user_beer.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: user_beers
+#
+#  id       :bigint           not null, primary key
+#  favorite :boolean          default(FALSE)
+#  seen_at  :datetime
+#  beer_id  :bigint
+#  user_id  :bigint
+#
+# Indexes
+#
+#  index_user_beers_on_beer_id  (beer_id)
+#  index_user_beers_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (beer_id => beers.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class UserBeer < ApplicationRecord
+  belons_to :user
+  belongs_to :beer
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string
+#  last_name       :string
+#  name            :string
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
 class UserSerializer < ActiveModel::Serializer
   attributes :name, :last_name, :full_name, :username, :token
 
